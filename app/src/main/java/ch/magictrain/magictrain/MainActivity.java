260fce5ff7dce0bfc39c57e9483d14973e3a82bf
Andreas <andreas@andreas>
2016-03-12 18:37:02 +0100

    fb2

--- app/src/main/java/ch/magictrain/magictrain/MainActivity.java
+++ app/src/main/java/ch/magictrain/magictrain/MainActivity.java
@@ -7,24 +7,44 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.estimote.sdk.SystemRequirementsChecker;
 
 import ch.magictrain.magictrain.models.UpdateResponse;
 import ch.magictrain.magictrain.views.TrainListView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
     private TrainListView list;
     private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+        //LoginButton loginButton = (LoginButton) R.id.login_button;
+        //loginButton.registerCallback(callbackManager,
+        //        new FacebookCallback<LoginResult>() { /*toto*/
+        //        });
+
         setContentView(R.layout.activity_main);
         list = (TrainListView) findViewById(R.id.trainListView);
         progress = (ProgressBar) findViewById(R.id.loading);
