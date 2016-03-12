bbc1fa2605f4c443704f09d7c9b3b959a3eb6fb2
Jonathan Müller <j.mueller@apoveda.ch>
2016-03-12 20:18:15 +0100

    add share button

---
+++ app/src/main/java/ch/magictrain/magictrain/TrainActivity.java
@@ -0,0 +1,33 @@
+package ch.magictrain.magictrain;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TrainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_train);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hi, I am sitting in IC841 Wagon 3.");
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "Share to..."));
+            }
+        });
+    }
+}
