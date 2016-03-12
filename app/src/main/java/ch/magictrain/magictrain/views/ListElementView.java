74be6eef43b5e681188630b7f49a1a0b5ee8fd81
najiji <thomas.debrunner@hotmail.com>
2016-03-12 15:58:06 +0100

    improved ui

--- app/src/main/java/ch/magictrain/magictrain/views/ListElementView.java
+++ app/src/main/java/ch/magictrain/magictrain/views/ListElementView.java
@@ -9,19 +9,28 @@ import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.skyfishjy.library.RippleBackground;
+
 import ch.magictrain.magictrain.R;
 import ch.magictrain.magictrain.Settings;
+import ch.magictrain.magictrain.models.Friend;
 
 public class ListElementView extends RelativeLayout {
     private TrainListView.ListElement data;
     private TextView nameTextView;
     private TextView carriageNrTextView;
+    private TextView decksTextView;
+    private TextView klassTextView;
     private ImageView background;
+    private LinearLayout bounceContainer;
 
     public void setData(TrainListView.ListElement data) {
         this.data = data;
@@ -47,8 +56,54 @@ public class ListElementView extends RelativeLayout {
     }
 
     private void updateViews() {
-        nameTextView.setText(data.toString());
+        // create names string
+        StringBuilder displayString = new StringBuilder();
+        int numberOfPeople = 0;
+        if(data.myLocation.isPresent()) {
+            numberOfPeople ++;
+            displayString.append("Du");
+            if(data.carriage.decks > 1) {
+                displayString.append(data.myLocation.get().deck == 1? " (unten)":" (oben)");
+            }
+        }
+        for(Friend friend: data.friends) {
+            numberOfPeople ++;
+            if(displayString.length() != 0) {
+                displayString.append(", ");
+            }
+            displayString.append(friend.fb_name);
+            if(data.carriage.decks > 1) {
+                displayString.append(friend.location.deck == 1? " (unten)":" (oben)");
+            }
+        }
+        nameTextView.setText(displayString);
         background.setImageDrawable(getDrawable());
+
+
+        bounceContainer.removeAllViews();
+        // insert the bounce things
+        for(int i=0; i<numberOfPeople; i++) {
+            RippleBackground ripple = (RippleBackground)LayoutInflater.from(getContext()).inflate(R.layout.bounce, null);
+            ripple.startRippleAnimation();
+            bounceContainer.addView(ripple);
+            bounceContainer.requestLayout();
+            ripple.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+            ripple.getLayoutParams().width = 140;
+        }
+
+        // update carriage number
+        if(data.isCarriageBegin) {
+            carriageNrTextView.setText("Wagen "+ (data.id));
+            decksTextView.setText(data.carriage.decks >1?"Doppelstöckig":"Einstöckig");
+            klassTextView.setText(Integer.toString(data.carriage.klass) + ". Klasse");
+        }
+        else {
+            carriageNrTextView.setText("");
+            decksTextView.setText("");
+            klassTextView.setText("");
+        }
+
+
     }
 
     public ListElementView(Context context) {
@@ -71,5 +126,8 @@ public class ListElementView extends RelativeLayout {
         nameTextView = (TextView)getRootView().findViewById(R.id.nameTextView);
         carriageNrTextView = (TextView)getRootView().findViewById(R.id.carriageNrTextView);
         background = (ImageView) getRootView().findViewById(R.id.background);
+        bounceContainer = (LinearLayout) getRootView().findViewById(R.id.bounceContainer);
+        decksTextView = (TextView) getRootView().findViewById(R.id.decksTextView);
+        klassTextView = (TextView) getRootView().findViewById(R.id.klassTextView);
     }
 }
