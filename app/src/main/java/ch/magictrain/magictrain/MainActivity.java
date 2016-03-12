f878c0f035b550198ac058e376884a1232c3d924
Andreas <andreas@andreas>
2016-03-12 20:28:58 +0100

    have user_id in shared pref

--- app/src/main/java/ch/magictrain/magictrain/MainActivity.java
+++ app/src/main/java/ch/magictrain/magictrain/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,22 +18,35 @@ import com.estimote.sdk.SystemRequirementsChecker;
 import ch.magictrain.magictrain.models.UpdateResponse;
 import ch.magictrain.magictrain.views.TrainListView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
 
+    //shared
+    public static final String PREFS_NAME = "FbPref";
+
     private TrainListView list;
     private ProgressBar progress;
     private LoginButton loginButton;
+
+    private String fb_id = "";
+    private String fb_fname = "";
+    private String fb_sname = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +60,39 @@ public class MainActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
         loginButton.registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        AccessToken accessToken = loginResult.getAccessToken();
+                        GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback(){
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                if (response.getError()!=null)
+                                {
+                                    Log.e("andreas","Error in Response "+ response);
+                                }
+                                else
+                                {
+                                    String email=object.optString("email");
+                                    Log.e("andreas" ,"Json Object Data "+object+" Email id "+ email);
+                                }
+                            }});
                         // App code
                         Log.d("andreas", "Callback in onCreateonSuccess");
+                        fb_id = accessToken.getUserId();
+                        fb_fname = "testfname";
+                        fb_sname = "testsname";
+                        Log.d("andreas", fb_id);
+
+                        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString("fb_id", fb_id);
+                        editor.putString("fb_fname", fb_fname);
+                        editor.putString("fb_sname", fb_sname);
+                        editor.commit();
+
                     }
 
                     @Override
@@ -73,7 +114,18 @@ public class MainActivity extends AppCompatActivity {
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
 
         startBackgroundService();
+
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        fb_id = settings.getString("fb_id", "");
+        Log.d("andreas", fb_id);
+        fb_fname = settings.getString("fb_fname", "");
+        Log.d("andreas", fb_fname);
+        fb_sname = settings.getString("fb_sname", "");
+        Log.d("andreas", fb_sname);
+
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
